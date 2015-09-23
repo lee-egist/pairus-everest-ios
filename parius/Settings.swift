e4d4b770a8355ae02ea4e71c86f2f8364755c77b
[01;31m[K"name"[m[K: "Erick Gist <[01;31m[K"email"[m[K: null>
2015-09-22 22:38:19 -0700

    and file groups so it's easier to find it. stubbed out the next steps.

--- parius/Settings.swift
+++ parius/Settings.swift
@@ -9,7 +9,15 @@
 import Foundation
 
 class Settings {
+//    get requests
     var viewTopics = "https://api.github.com/users/mralexgray/repos"
-    
+//    var viewTopics = "https://sheltered-coast-5550.herokuapp.com/api/v1/topics"
+    var login = "https://sheltered-coast-5550.herokuapp.com/api/v1/login"
+    var logout = "https://sheltered-coast-5550.herokuapp.com/api/v1/logout"
+    var getSchedule = "https://sheltered-coast-5550.herokuapp.com/api/v1/schedule"
+    var getPotentialPairs = "https://sheltered-coast-5550.herokuapp.com/api/v1/availabilities"
+//    post request
+    var sendFavorites = "https://sheltered-coast-5550.herokuapp.com/api/v1/potentialpairs"
+    var createTopic = "https://sheltered-coast-5550.herokuapp.com/api/v1/topic.create"
     
 }
