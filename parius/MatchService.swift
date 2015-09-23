ff851c770434a6dd07593ed3ec9ea2833fbb5273
Tarik B <tarik.bennett@gmail.com>
2015-09-23 14:40:16 -0700

    Add Match, Availability controllers. Add Match, AvailabilityService files. Add settings URLs

---
+++ parius/MatchService.swift
@@ -0,0 +1,40 @@
+//
+//  MatchService.swift
+//  parius
+//
+//  Created by Apprentice on 9/23/15.
+//  Copyright © 2015 com.RAREStudios. All rights reserved.
+//
+
+import Foundation
+
+class MatchService {
+    
+    var settings: Settings!
+    
+    init() {
+        self.settings = Settings()
+    }
+    
+    func getMatches(callback:(NSArray) -> ()) {
+        request(settings.viewMatches, callback: callback)
+    }
+    
+    func request(url:String, callback:(NSArray) -> ()) {
+        
+        let nsURL = NSURL(string: url)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(nsURL!, completionHandler: { data, response, error -> Void in
+            if (error != nil) {
+                print(error!.localizedDescription)
+            }
+            do {
+                let response = try NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.MutableContainers) as! NSArray
+                callback(response)
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
+    }
+}
--- parius/Settings.swift
+++ parius/Settings.swift
@@ -9,16 +9,19 @@
 import Foundation
 
 class Settings {
-        
+  
+    // get routes
     var viewTopics = "https://localhost:3000/api/v1/groups/topics"
     var viewHours = "https://sheltered-coast-5550.herokuapp.com/api/v1/schedule"
+    var viewAvailabilities = "https://sheltered-coast-5550.herokuapp.com/api/v1/availabilities"
+    var viewMatches = "https://sheltered-coast-5550.herokuapp.com/api/v1/potentialpairs"
 
-    
+
+    // login routes
     var login = "https://sheltered-coast-5550.herokuapp.com/api/v1/login"
     var logout = "https://sheltered-coast-5550.herokuapp.com/api/v1/logout"
-    var getPotentialPairs = "https://sheltered-coast-5550.herokuapp.com/api/v1/availabilities"
     
-    
+    // post routes
     var sendFavorites = "https://sheltered-coast-5550.herokuapp.com/api/v1/potentialpairs"
     var createTopic = "https://sheltered-coast-5550.herokuapp.com/api/v1/topic.create"
     
